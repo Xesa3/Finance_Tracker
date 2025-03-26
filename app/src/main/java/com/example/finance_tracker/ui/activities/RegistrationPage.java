d47dc20ce9b1da5c467acf8989963d017f2fbcc4
Xesa <minecrta@yandex.ru>
2025-03-26 19:22:04 +0300

    Настроил проект

---
+++ app/src/main/java/com/example/finance_tracker/ui/activities/RegistrationPage.java
@@ -0,0 +1,45 @@
+package com.example.finance_tracker.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.View;
+
+import com.example.finance_tracker.R;
+
+public class RegistrationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment); // Меняем контейнер на фрагмент
+        transaction.commit();
+    }
+
+    public void clickLogin(View view){
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
+}
